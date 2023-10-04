95a5cf53cd9b2406b3bb4e69314f7ef6b8a78de7
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-10-04 23:02:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// Display a confirmation dialog
+const proceed = confirm("Do you want to proceed?");
+
+// Check the user's choice
+if (proceed) {
+  // User clicked OK
+  alert("You clicked OK. Proceeding...");
+} else {
+  // User clicked Cancel
+  alert("You clicked Cancel. Exiting...");
+}
